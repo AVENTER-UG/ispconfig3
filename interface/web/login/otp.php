0a843842db059039da677810a0f926197f1b129b
Herman van Rink <rink@initfour.nl>
2022-03-11 20:56:14 +0100

    resend with a d

--- interface/web/login/otp.php
+++ interface/web/login/otp.php
@@ -135,7 +135,7 @@ if($_SESSION['otp']['type'] == 'email') {
 	}
 
 	//* Send code via email
-       if(!isset($_SESSION['otp']['sent']) || $_GET['action'] == 'resent') {
+	if(!isset($_SESSION['otp']['sent']) || $_GET['action'] == 'resend') {
 
 		//* Ensure that code is not sent too often
 		if(isset($_SESSION['otp']['sent']) && $_SESSION['otp']['sent'] > $max_code_resend) {
