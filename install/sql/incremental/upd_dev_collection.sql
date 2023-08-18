85bcf5c6f44e9935c2f898bf18560017526dd17c
Herman van Rink <rink@initfour.nl>
2023-08-18 21:46:51 +0200

    Convert back to a timestamp
    works better with date()

--- install/sql/incremental/upd_dev_collection.sql
+++ install/sql/incremental/upd_dev_collection.sql
@@ -1,2 +1,2 @@
 
-ALTER TABLE `mail_user` ADD `last_access` timestamp NULL DEFAULT NULL after `disabledoveadm`;
+ALTER TABLE `mail_user` ADD `last_access` int(11) NULL DEFAULT NULL after `disabledoveadm`;
