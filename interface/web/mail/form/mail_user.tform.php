3aab298c478f19dac3b1a956f2cd8622f09e17fa
Riccardo Leonardi <riccardo@xtsystem.it>
2023-10-21 08:33:59 +0000

    Implements #6582 Autoresponder in HTML format

--- interface/web/mail/form/mail_user.tform.php
+++ interface/web/mail/form/mail_user.tform.php
@@ -381,7 +381,7 @@ if ($global_config['mail']['mailbox_show_autoresponder_tab'] === 'y') {
 				'formtype' => 'TEXTAREA',
 				'filters'   => array(
 					0 => array( 'event' => 'SAVE',
-					'type' => 'STRIPTAGS')
+					'type' => 'TRIM')
 			),
 				'default' => '',
 				'value'  => '',
@@ -389,6 +389,12 @@ if ($global_config['mail']['mailbox_show_autoresponder_tab'] === 'y') {
 				'rows'  => '15'
 			),
 			'autoresponder' => array (
+				'datatype' => 'VARCHAR',
+				'formtype' => 'CHECKBOX',
+				'default' => 'n',
+				'value'  => array(1 => 'y', 0 => 'n')
+			),
+			'autoresponder_html' => array (
 				'datatype' => 'VARCHAR',
 				'formtype' => 'CHECKBOX',
 				'default' => 'n',
