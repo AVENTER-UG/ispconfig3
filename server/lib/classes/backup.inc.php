d9121624335ed159e4c1fd9623a2fa3a8a4c8165
Christoph Zysik <support@ztk.me>
2024-04-23 14:49:30 +0000

    Fix potential script termination when using unchecked variable

--- server/lib/classes/backup.inc.php
+++ server/lib/classes/backup.inc.php
@@ -1197,22 +1197,26 @@ class backup
                 case 'borg':
                     $repos_path = $backup_dir . '/' . $entry;
                     if (is_dir($repos_path) && strncmp('borg_', $entry, 5) === 0) {
-                        $archivesJson = json_decode(implode("", self::getReposArchives($backup_mode, $repos_path, $password, 'json')), TRUE);
-                        foreach ($archivesJson['archives'] as $archive) {
-                            if (is_null($prefix_list)) { //fallback if no prefix_list
-                                $archives[] = [
-                                    'repos' => $entry,
-                                    'archive' => $archive['name'],
-                                    'created_at' => strtotime($archive['time']),
-                                ];
-                            } else {
-                                foreach ($prefix_list as $prefix) {
-                                    if (substr($archive['name'], 0, strlen($prefix)) == $prefix) { //filter backup list of all if no prefix_list
-                                        $archives[] = [
-                                            'repos' => $entry,
-                                            'archive' => $archive['name'],
-                                            'created_at' => strtotime($archive['time']),
-                                        ];
+                        $repos_archives = self::getReposArchives($backup_mode, $repos_path, $password, 'json');
+                        if(is_array($repos_archives))
+                        {
+                            $archivesJson = json_decode(implode("", $repos_archives), TRUE);
+                            foreach ($archivesJson['archives'] as $archive) {
+                                if (is_null($prefix_list)) { //fallback if no prefix_list
+                                    $archives[] = [
+                                        'repos' => $entry,
+                                        'archive' => $archive['name'],
+                                        'created_at' => strtotime($archive['time']),
+                                    ];
+                                } else {
+                                    foreach ($prefix_list as $prefix) {
+                                        if (substr($archive['name'], 0, strlen($prefix)) == $prefix) { //filter backup list of all if no prefix_list
+                                            $archives[] = [
+                                                'repos' => $entry,
+                                                'archive' => $archive['name'],
+                                                'created_at' => strtotime($archive['time']),
+                                            ];
+                                        }
                                     }
                                 }
                             }
